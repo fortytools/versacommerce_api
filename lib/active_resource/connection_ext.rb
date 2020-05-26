eb92ff859da84e6f723c32b6375d7b1e78ab904e
Sebastian M. Gauck <smg@42ls.de>
2020-05-26 22:25:55 +0200

    Get rid of alias_method_chain

--- lib/active_resource/connection_ext.rb
+++ lib/active_resource/connection_ext.rb
@@ -2,16 +2,11 @@
 require 'active_support/core_ext/module/aliasing'
 
 module ActiveResource
-  class Connection
-
+  module ConnectionWithDetailedLogSubscriber
     attr_reader :response
 
-    def handle_response_with_response_capture(response)
-      @response = handle_response_without_response_capture(response)
-    end
-
-    def request_with_detailed_log_subscriber(method, path, *arguments)
-      result = request_without_detailed_log_subscriber(method, path, *arguments)
+    def request(method, path, *arguments)
+      result = super(method, path, *arguments)
       detailed_log_subscriber(result, arguments)
       result
     rescue => e
@@ -25,8 +20,9 @@ module ActiveResource
         payload[:data]     = arguments
       end
     end
-
-    alias_method_chain :handle_response, :response_capture
-    alias_method_chain :request, :detailed_log_subscriber
   end
+  class Connection
+    prepend ConnectionWithDetailedLogSubscriber
+  end
+
 end
